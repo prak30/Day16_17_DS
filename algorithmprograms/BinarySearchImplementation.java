e2e0dbbdaca59c24279667872aab26299ff382ff
pranav <prnvkatkar88@gmail.com>
2021-10-24 19:42:03 +0530

    Added:Binary search

--- algorithmprograms/BinarySearchImplementation.java
+++ algorithmprograms/BinarySearchImplementation.java
@@ -5,7 +5,34 @@ import java.util.Arrays;
 public class BinarySearchImplementation {
 
 	public static void main(String[] args) {
+		String[] arr = { "Lion", "Tiger", "Elephant", "Zebra", "Giraffe" };
+		Arrays.sort(arr);
+//		System.out.println(Arrays.toString(arr));
+		String a = "Tiger";
+		int result = binarySearch(arr, a);
 
-	System.out.println("Welcome to data structures algorithms");
+		if (result == -1) {
+			System.out.println("NOT FOUND");
+		} else {
+			System.out.println("FOUND");
+		}
 	}
+
+	static int binarySearch(String[] arr, String a) {
+		int start = 0;
+		int end = arr.length - 1;
+		while (start <= end) {
+			int mid = (start + end) / 2;
+			int ans = a.compareTo(arr[mid]);
+			if (ans == 0)
+				return mid;
+			if (ans > 0)
+				start = mid + 1;
+			else
+				end = mid - 1;
+		}
+		return -1;
+
+	}
+
 }
