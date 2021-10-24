0851e94f600129114cbf54d5eb1a03e50668799b
pranav <prnvkatkar88@gmail.com>
2021-10-24 19:36:28 +0530

    Added:welcome message

---
+++ algorithmprograms/BinarySearchImplementation.java
@@ -0,0 +1,11 @@
+package algorithmprograms;
+
+import java.util.Arrays;
+
+public class BinarySearchImplementation {
+
+	public static void main(String[] args) {
+
+	System.out.println("Welcome to data structures algorithms");
+	}
+}
